7dfc850587c4185c856f9228bde1a8c2f8e05b0e
Cursed-Coder777 <cursedcoder812@gmail.com>
2026-03-10 12:30:53 -0700

    automatically redirect to /home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>Home</div>
+  redirect('/home')
+  return(
+    <div className='flex justify-center items-center h-screen'>
+   <Link className='bg-blue-400 text-white p-4 rounded-lg' href="/home">
+      Go to Home
+    </Link>
+  </div>
   )
 }
 
