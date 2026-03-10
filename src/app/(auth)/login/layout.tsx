bf14b4428849b421e7c4fc5ba80b944588d13bb8
Cursed-Coder777 <cursedcoder812@gmail.com>
2026-03-10 12:04:29 -0700

    navbar me suspense add kya

--- src/app/(auth)/login/layout.tsx
+++ src/app/(auth)/login/layout.tsx
@@ -1,14 +1,16 @@
 'use client'
 import Navbar from '@/components/myComponents/Navbar'
 import Sidebar from '@/components/myComponents/Sidebar'
-import React, { ReactNode } from 'react'
+import React, { ReactNode, Suspense } from 'react'
 const Layout = ({ children }: { children: ReactNode }) => {
 
     return (
         <div className="flex">
             <div className="w-[3%]"> <Sidebar /></div>
             <div className="flex flex-col w-[97%]">
-                <Navbar />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Navbar />
+                </Suspense>
                 {children}
             </div></div>
     )
